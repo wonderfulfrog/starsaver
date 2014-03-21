02607591280905b068e74eceff2943cbf6fcf792
Devin Lumley <keiroshin@gmail.com>
2014-03-21 13:59:55 -0700

    Initial commit

---
+++ js/starsaver.js
@@ -0,0 +1,21 @@
+var WIDTH = window.innerWidth;
+var HEIGHT = window.innerHeight;
+
+var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.CANVAS, 'starsaver', {create: create, update: update, render: render });
+
+var stars;
+
+function create() {
+
+    stars = [];
+
+}
+
+function update() {
+
+}
+
+function render() {
+
+
+}
